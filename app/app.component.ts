a673f8cf6caa67fffe747d76cf41371c6b5c1c29
N, Thangadurai <duraithanga3@gmail.com>
2016-08-11 16:51:36 +0530

    Initial commit with angular 2 and typescript

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hi Thangadurai, Your angular 2 app is running fine.</h1>'
+})
+
+export class AppComponent { }
